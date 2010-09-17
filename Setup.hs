88b1bbd46db3e4e9091ee26beb7342633ca6411d
Gabriel Wicke <wicke@wikidev.net>
2010-09-17 09:18:59 +0200

    Add a test for trailingZeros

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,19 @@
+{-# LANGUAGE CPP #-}
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo(LocalBuildInfo, buildDir)
+import System.Cmd(system)
+
+ps :: String
+#if mingw32_HOST_OS || mingw32_TARGET_OS
+ps = ['\\']
+#else
+ps = ['/']
+#endif
+
+main = defaultMainWithHooks hooks
+  where hooks = simpleUserHooks { runTests = runTests' }
+
+runTests' :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
+runTests' _ _ _ lbi = system testprog >> return ()
+  where testprog = (buildDir lbi) ++ ps ++ "test" ++ ps ++ "test"
---
+++ test/test.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE ForeignFunctionInterface, RankNTypes, FlexibleContexts, ExistentialQuantification #-}
+-- Test framework imports
+import Test.Framework (defaultMain, testGroup, Test)
+import Test.Framework.Providers.HUnit
+import Test.HUnit hiding (Test, Testable)
+import Test.Framework.Providers.QuickCheck2
+import Test.QuickCheck
+
+-- Code to test
+import Data.Bits.Extras
+import Data.Word
+import Data.Int
+import Data.Bits
+
+tests :: [Test]
+tests = [ testGroup "Test Cases" 
+          [ testCase "trailingZeros" test_trailingZeros_all
+          ]
+        ]
+
+main :: IO ()
+main = defaultMain tests
+
+-- TODO: Use Quickcheck to produce arbitrary values?
+testPattern :: Integral a => a
+testPattern = 0xdeadbeef
+
+
+type PolyTest = (ExtraBits a, Integral a, Bounded a) => a -> Assertion
+
+testTypes :: PolyTest -> Assertion
+testTypes t = do
+    t (testPattern :: Int)
+    t (testPattern :: Word)
+    t (testPattern :: Int8)
+    t (testPattern :: Int16)
+    t (testPattern :: Int32)
+    t (testPattern :: Int64)
+    t (testPattern :: Word8)
+    t (testPattern :: Word16)
+    t (testPattern :: Word32)
+    t (testPattern :: Word64)
+
+test_trailingZeros :: PolyTest
+test_trailingZeros i = do
+    let t0 = i - i
+    assertEqual "0" (fromIntegral t0) (trailingZeros t0)
+    trailingZeros (t0 + 1) @?= fromIntegral t0
+    trailingZeros (t0 + 3) @?= fromIntegral t0
+    trailingZeros (t0 + 2) @?= fromIntegral t0 + 1
+    let hbitshift = bitSize i - 1
+        hbitset = (t0 + 1) `shiftL` hbitshift
+    assertEqual "High bit set" (fromIntegral hbitset) hbitshift
+test_trailingZeros_all :: Assertion
+test_trailingZeros_all = testTypes test_trailingZeros
